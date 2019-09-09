6602b2113a03cb90d4ba46eb1670140be5a3e938
Maksym Vereshchaka <max111095@gmail.com>
2019-09-09 20:10:15 +0300

    initial commit

---
+++ NytimesTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NytimesTest
+//
+//  Created by DeveloperMBPRO on 9/9/19.
+//  Copyright © 2019 Test project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
